1b36162f4e4890c39318d2f543a48d3f14ab3c47
Dan Branton <dtotheb@gmail.com>
2012-04-01 16:16:55 -0400

    setup templates for index view, which just returns the result of "ls -la"

--- FlaskControl.py
+++ FlaskControl.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+import subprocess
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    out = subprocess.check_output(['ls', '-la'])
+    return render_template('output.html', output=out)
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
